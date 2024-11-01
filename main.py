0f579d1e50fcf31e498ddf44d6734423836e688e
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-11-01 18:53:35 +0100

    refactor(main): add def main function for better code structure and remove useless exit method

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from src.ImageProcessor import ImageProcessor
 
-try:
-    folder_path = input("Enter the path to the folder containing the images: ")
-    processor = ImageProcessor(folder_path)
 
-    new_size = int(input("Enter the new size for the images: "))
-    processor.process_folder(new_size)
-except Exception as e:
-    print("An error occurred: ", e)
-    exit()
+def main():
+    try:
+        folder_path = input("Enter the path to the folder containing the images: ")
+        processor = ImageProcessor(folder_path)
+
+        new_size = int(input("Enter the new size for the images: "))
+        processor.process_folder(new_size)
+    except Exception as e:
+        print("An error occurred: ", e)
+
+
+if __name__ == "__main__":
+    main()
